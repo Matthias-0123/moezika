4a5170a7ebb888e40f5b87b2db67c2b7eabf0e8d
Matthias-0123 <shawkimatthias@gmail.com>
2025-05-05 15:14:22 +0200

    Main changes full application music application

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,421 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: audio());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class audio extends StatefulWidget {
+  const audio({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _audioState createState() => _audioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _audioState extends State<audio> {
+  final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final Playlist _playlist = Playlist(
+    audios: [
+      Audio(
+        "assets/audios/nokia.mp3",
+        metas: Metas(
+          title: "Nokia",
+          image: MetasImage.asset("assets/images/nokia.jpg"),
+        ),
+      ),
+      Audio(
+        "assets/audios/hood.mp3",
+        metas: Metas(
+          title: "Hood",
+          image: MetasImage.asset("assets/images/hood.jpg"),
+        ),
+      ),
+      Audio(
+        "assets/audios/toxic.mp3",
+        metas: Metas(
+          title: "Toxic",
+          image: MetasImage.asset("assets/images/toxic.jpg"),
+        ),
+      ),
+    ],
+  );
+
+  String _currentTitle = "No song playing";
+  String _currentImage = "assets/images/default.jpg";
+
+  @override
+  void initState() {
+    super.initState();
+    _assetsAudioPlayer.open(
+      _playlist,
+      loopMode: LoopMode.playlist,
+      autoStart: false,
+    );
+    _assetsAudioPlayer.current.listen((playing) {
+      if (playing != null) {
+        setState(() {
+          _currentTitle = playing.audio.audio.metas.title ?? "No song playing";
+          _currentImage =
+              (playing.audio.audio.metas.image?.path ??
+                  "assets/images/default.jpg");
+        });
+      }
     });
   }
 
+  // Play audio
+  void _playAudio() {
+    _assetsAudioPlayer.play();
+  }
+
+  // Stop audio
+  void _stopAudio() {
+    _assetsAudioPlayer.stop();
+  }
+
+  void _nextAudio() {
+    _assetsAudioPlayer.next();
+  }
+
+  void _previousAudio() {
+    _assetsAudioPlayer.previous();
+  }
+
+  void _playAudioAtIndex(int index) {
+    _assetsAudioPlayer.playlistPlayAtIndex(index);
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       centerTitle: true,
+  //       title: const Text(
+  //         'Audio player Application',
+  //         style: TextStyle(fontSize: 25),
+  //       ),
+  //       backgroundColor: Colors.green[700],
+  //       toolbarHeight: 70,
+  //       elevation: 5,
+  //       shadowColor: Colors.green[700],
+  //       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+  //     ),
+  //     body: Center(
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         spacing: 10,
+  //         children: [
+  //           Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: [Text("Playlist")],
+  //           ),
+  //           Column(
+  //             mainAxisAlignment: MainAxisAlignment.center,
+  //             children: <Widget>[
+  //               Image.asset(_currentImage, width: 200, height: 400),
+  //               const SizedBox(height: 6),
+  //               Text(
+  //                 'Currently Playing: $_currentTitle',
+  //                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+  //               ),
+  //               const SizedBox(height: 20),
+  //               Row(
+  //                 spacing: 3,
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 children: [
+  //                   ElevatedButton(
+  //                     onPressed: _playAudio,
+  //                     style: ElevatedButton.styleFrom(
+  //                       backgroundColor: Colors.green[700],
+  //                       shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(10),
+  //                       ),
+  //                       padding: EdgeInsets.only(
+  //                         right: 20,
+  //                         left: 25,
+  //                         top: 5,
+  //                         bottom: 15,
+  //                       ),
+  //                     ),
+  //                     child: const Text(
+  //                       'Play',
+  //                       style: TextStyle(
+  //                         fontSize: 10,
+  //                         letterSpacing: 1.0,
+  //                         fontWeight: FontWeight.bold,
+  //                         color: Colors.black,
+  //                       ),
+  //                       textAlign: TextAlign.center,
+  //                     ),
+  //                   ),
+  //                   SizedBox(height: 20),
+  //                   ElevatedButton(
+  //                     onPressed: _stopAudio,
+  //                     style: ElevatedButton.styleFrom(
+  //                       backgroundColor: Colors.green[700],
+  //                       shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(10),
+  //                       ),
+  //                       padding: EdgeInsets.only(
+  //                         right: 20,
+  //                         left: 25,
+  //                         top: 15,
+  //                         bottom: 15,
+  //                       ),
+  //                     ),
+
+  //                     child: const Text(
+  //                       'Stop',
+  //                       style: TextStyle(
+  //                         fontSize: 10,
+  //                         letterSpacing: 1.0,
+  //                         fontWeight: FontWeight.bold,
+  //                         color: Colors.black,
+  //                       ),
+  //                       textAlign: TextAlign.center,
+  //                     ),
+  //                   ),
+  //                   const SizedBox(height: 20),
+  //                   ElevatedButton(
+  //                     onPressed: _nextAudio,
+  //                     style: ElevatedButton.styleFrom(
+  //                       backgroundColor: Colors.green[700],
+  //                       shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(10),
+  //                       ),
+  //                       padding: EdgeInsets.only(
+  //                         right: 20,
+  //                         left: 25,
+  //                         top: 15,
+  //                         bottom: 15,
+  //                       ),
+  //                     ),
+  //                     child: const Text(
+  //                       'Next',
+  //                       style: TextStyle(
+  //                         fontSize: 10,
+  //                         letterSpacing: 1.0,
+  //                         fontWeight: FontWeight.bold,
+  //                         color: Colors.black,
+  //                       ),
+  //                       textAlign: TextAlign.center,
+  //                     ),
+  //                   ),
+  //                   const SizedBox(height: 20),
+  //                   ElevatedButton(
+  //                     onPressed: _previousAudio,
+  //                     style: ElevatedButton.styleFrom(
+  //                       backgroundColor: Colors.green[700],
+  //                       shape: RoundedRectangleBorder(
+  //                         borderRadius: BorderRadius.circular(10),
+  //                       ),
+  //                       padding: EdgeInsets.only(
+  //                         right: 20,
+  //                         left: 25,
+  //                         top: 15,
+  //                         bottom: 15,
+  //                       ),
+  //                     ),
+  //                     child: const Text(
+  //                       'Previous',
+  //                       style: TextStyle(
+  //                         fontSize: 10,
+  //                         letterSpacing: 1.0,
+  //                         fontWeight: FontWeight.bold,
+  //                         color: Colors.black,
+  //                       ),
+  //                       textAlign: TextAlign.center,
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ],
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        title: const Text(
+          'Audio Player Application',
+          style: TextStyle(fontSize: 25),
         ),
+        backgroundColor: Colors.green[700],
+        toolbarHeight: 70,
+        elevation: 5,
+        shadowColor: Colors.green[700],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Row(
+        children: [
+          // Column for currently playing song and image
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  _currentImage,
+                  width: 200,
+                  height: 200,
+                ), // Dynamic image
+                const SizedBox(height: 10),
+                Text(
+                  'Currently Playing:',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  _currentTitle,
+                  style: const TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _previousAudio,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      child: const Text(
+                        'Previous',
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: _playAudio,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      child: const Text(
+                        'Play',
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: _stopAudio,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      child: const Text(
+                        'Stop',
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: _nextAudio,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      child: const Text(
+                        'Next',
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // Column for playlist
+          Expanded(
+            flex: 1,
+            child: ListView.builder(
+              itemCount: _playlist.audios.length,
+              itemBuilder: (context, index) {
+                final audio = _playlist.audios[index];
+
+                return StreamBuilder<Playing?>(
+                  stream: _assetsAudioPlayer.current,
+                  builder: (context, snapshot) {
+                    final isSelected =
+                        snapshot.data?.audio.audio.metas.title ==
+                        audio.metas.title;
+
+                    return ListTile(
+                      title: Text(
+                        audio.metas.title ?? "Unknown Title",
+                        style: TextStyle(
+                          fontWeight:
+                              isSelected ? FontWeight.bold : FontWeight.normal,
+                          color: isSelected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                      tileColor:
+                          isSelected ? Colors.green[700] : Colors.grey[200],
+                      onTap: () {
+                        _playAudioAtIndex(index);
+                      },
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _assetsAudioPlayer.dispose();
+    super.dispose();
   }
 }
